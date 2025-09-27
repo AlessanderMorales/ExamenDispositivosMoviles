649c494bbd3dad5c7b91e3c5a2c5ede20d4099f4
AlessanderMorales <alessmoralesmo@gmail.com>
2025-09-27 09:23:05 -0400

    Agregano Entidad Dolar

--- app/src/main/java/com/calyrsoft/ucbp1/navigation/AppNavigation.kt
+++ app/src/main/java/com/calyrsoft/ucbp1/navigation/AppNavigation.kt
@@ -18,7 +18,7 @@ fun AppNavigation() {
 
     NavHost(
         navController = navController,
-        startDestination = Screen.Github.route
+        startDestination = Screen.PopularMovies.route
     ) {
         composable(Screen.Github.route) {
             GithubScreen(modifier = Modifier)
