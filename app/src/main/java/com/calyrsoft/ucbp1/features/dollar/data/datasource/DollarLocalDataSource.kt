7f8ac8bf8620066b10be5ca2cf7eca1f7305d7ec
AlessanderMorales <alessmoralesmo@gmail.com>
2025-09-27 09:51:55 -0400

    Modificacion en el DataSource

--- app/src/main/java/com/calyrsoft/ucbp1/features/dollar/data/datasource/DollarLocalDataSource.kt
+++ app/src/main/java/com/calyrsoft/ucbp1/features/dollar/data/datasource/DollarLocalDataSource.kt
@@ -6,25 +6,22 @@ import com.calyrsoft.ucbp1.features.dollar.data.mapper.toModel
 import com.calyrsoft.ucbp1.features.dollar.domain.model.DollarModel
 
 class DollarLocalDataSource(
-    val dao: IDollarDao
+    private val dao: IDollarDao
 ) {
 
     suspend fun getList(): List<DollarModel> {
-        return dao.getList().map {
-            it.toModel()
-        }
-
+        return dao.getList().map { it.toModel() }
     }
     suspend fun deleteAll() {
         dao.deleteAll()
     }
-    suspend fun inserTDollars(list: List<DollarModel>) {
-        val dollarEntity = list.map { it.toEntity() }
-        dao.insertDollars(dollarEntity)
+
+    suspend fun insertDollars(list: List<DollarModel>) {
+        val dollarEntities = list.map { it.toEntity() }
+        dao.insertDollars(dollarEntities)
     }
 
     suspend fun insert(dollar: DollarModel) {
         dao.insert(dollar.toEntity())
     }
-
 }
