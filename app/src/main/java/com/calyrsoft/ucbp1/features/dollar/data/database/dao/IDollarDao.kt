2c3b0b28ab087b3f40fadffc1192067637dec6d8
AlessanderMorales <alessmoralesmo@gmail.com>
2025-09-27 09:43:59 -0400

    Modificaciones IDollar

--- app/src/main/java/com/calyrsoft/ucbp1/features/dollar/data/database/dao/IDollarDao.kt
+++ app/src/main/java/com/calyrsoft/ucbp1/features/dollar/data/database/dao/IDollarDao.kt
@@ -6,20 +6,23 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.calyrsoft.ucbp1.features.dollar.data.database.entity.DollarEntity
 
-@Dao
 interface IDollarDao {
+
     @Query("SELECT * FROM dollars")
     suspend fun getList(): List<DollarEntity>
-
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(dollar: DollarEntity)
 
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertDollars(lists: List<DollarEntity>)
 
     @Query("DELETE FROM dollars")
     suspend fun deleteAll()
 
+    @Query("SELECT * FROM dollars ORDER BY timestamp DESC")
+    suspend fun getAllOrderedByDate(): List<DollarEntity>
 
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insertDollars(lists: List<DollarEntity>)
+    @Query("DELETE FROM dollars WHERE id = :id")
+    suspend fun deleteById(id: Int)
 }
